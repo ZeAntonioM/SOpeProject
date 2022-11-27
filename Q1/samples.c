ddc9d83916f0104c42a1b03599badf52788155fb
dsantosferreira <unidsferreira2003@gmail.com>
2022-11-27 21:13:43 +0000

    First exercice probably done

--- Q1/samples.c
+++ Q1/samples.c
@@ -13,38 +13,88 @@
 // nao mostramos e contamos a new line
 // 3- 
 
-
+long verifyInput(char* arg, long *n) {
+    char* endptr;
+    *n = strtol(arg, &endptr, 10);
+    
+    return *endptr == '\0';
+}
 
 int main(int argc, char* argv[]) {
-    
-    //Checks if number of arguments is correct (Argv[], argc, n, m)
+    FILE* file;
+    char *theStr;
+    int alreadyStored, arraySize = 0;
+    long numberFrags, maxFragSize, test1, test2, tam, idx;
+    long *alreadySeen; // Que capacidade?
+    srandom(0);
+
+    // Checks if number of arguments is correct (argv[], argc, n, m)
     if (argc != 4) {
-        printf("usage: samples file numberfrags maxfragsize");
-        return 1;
+        printf("Invalid number of arguments\nUsage: samples file numberfrags maxfragsize\n");
+        return EXIT_FAILURE;
     }
 
-
-    FILE* file, test;
-    char* theStr;
-    int numberfrags, maxfragsize, tam;
-    //
-    srandom(0);
+    // Checks if the last two arguments are numbers
+    if (!verifyInput(argv[2], &numberFrags) || !verifyInput(argv[3], &maxFragSize)) {
+        printf("numberfrags and maxfragsize must be integers\n");
+        return EXIT_FAILURE;
+    }
     
     file = fopen(argv[1], "r");
     if (file == NULL) {
-        printf("Oh esupido do caralho mete a puta dum ficheiro men! pfv ;) :P :D B) xD :') :( ");
+        printf("Provided file argument isn't a file\n");
+        return EXIT_FAILURE;
     }
-    int test1 = ftell(file);
-    fseek(file, 0, SEEK_END);
-    int test2 = ftell(file);
-    printf("%d\n", test2 - test1);
-    tam = test2 - test1;
 
-    if ( tam < ( atoi(argv[2]) + atoi(argv[3]) - 1 ) ) {
-        printf("Dava jeito uma string em condições oh caralho, não peças merdas que não fazem sentido zézé ");
-        return 1;
+    test1 = ftell(file);
+    fseek(file, 0, SEEK_END);
+    test2 = ftell(file);
+    tam = test2 - test1; // Rever tam
+
+    if (tam < numberFrags + maxFragSize - 1) {
+        printf("Can't get number of number of different fragments you asked with the fragment provided\n");
+        return EXIT_FAILURE;
     }
-    
+
+    // Getting the fragments
+    theStr = (char*) malloc(maxFragSize * sizeof(char));
+    alreadySeen = (long*) malloc((tam-maxFragSize) * sizeof(long));
+
+    while (numberFrags != 0) {
+        long aTry;
+
+        // Determinar inicio de proximo fragmento
+        while (1) {
+            alreadyStored = 0;
+            aTry = random() % (tam-maxFragSize); // Rever isto
+            for (int i = 0; i < arraySize; i++) {
+                if (aTry == alreadySeen[i]) {
+                    alreadyStored = 1;
+                    break;
+                }
+            }
+            if (!alreadyStored)
+                break;
+        }
+        
+        fseek(file, aTry, SEEK_SET);
+        fread(theStr, maxFragSize, 1, file);
+
+        // Print the string
+        idx = 0;
+        printf(">");
+        while (*(theStr + idx) != '\0') {
+            if (*(theStr + idx) == '\n')
+                printf(" ");
+            else
+                printf("%c", *(theStr + idx));
+            idx++;
+        }
+        printf("<\n");
+        alreadySeen[arraySize++] = aTry;
+        numberFrags--;
+    }
+
     return 0;
 
 }
