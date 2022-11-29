31a48e5656a8ec1ff4c6fe14b861d98003defd74
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2022-11-29 04:28:00 +0000

    Finishing ex2

--- Q2/txt2epub.c
+++ Q2/txt2epub.c
@@ -8,7 +8,7 @@
 
 
 
-// Changes the name of the file from ".txt" to ".epub"
+// Changes the extension of the file from ".txt" to ".epub"
  void changeNameToEpub( char* name){
     int size = strlen(name);
     int counter = 0;
@@ -27,77 +27,67 @@
     *name='b';
 }
 
-//BRO JUNTEM AS CENAS MALTAAAAAAAAAAAAA
-
-//Create the function to pass as argument for the exec
-void createFunc(char* func, char* argv, char* filesEmEpub){
-   // strcat(func,"pandoc ");
-    strcat(func,argv);
-    strcat(func," -o ");
-    strcat(func, filesEmEpub);
-}
 
 int main(int argc, char* argv[]) {
-   /*
-    char test[]= "JoeMama\0";
-    *test = '4';
-    printf("%s\n", test);
-    */
-
-    char* filesEmEpub[argc-1]; 
-    pid_t pideFilhos[argc-1];//Alá salazaratings
+   
+    char* filesInEpub[argc-1]; 
+    pid_t pidChilds[argc-1];
     int size_for_zip;
 
     // Creates child processes and execs the pandoc command on each of the children
     for (int i = 1; i < argc; i++) {
-        int pide = fork();
-
-        if (pide == 0) {
-            char * func= (char*) malloc((12+ 2*strlen(argv[i])) * sizeof(char)); // PERGUNTAR AO STOR COMO DAR FREE A ISTO???
-            createFunc(func, argv[i],filesEmEpub[i]);
-            execl("pandoc ", argv[i] ," -o ",filesEmEpub[i]); // pandoc f1.txt -o f1.epub
+        int pid = fork();
+         char *epub= (char*)malloc(strlen(argv[i]));
+            strcpy(epub,argv[i]);
+            changeNameToEpub(epub);
+            filesInEpub[i - 1] = epub;
+            size_for_zip += strlen(filesInEpub[i - 1]) + 1;
+        if(pid!=0) printf("[pide %d] converting %s\n", pid, argv[i]);
+        if (pid == 0) {
             
+            //char * func= (char*) malloc((12+ 2*strlen(argv[i])) * sizeof(char)); // PERGUNTAR AO STOR COMO DAR FREE A ISTO???
+            //createFunc(func, argv[i],filesEmEpub[i]);
+            char *args[]={"pandoc", argv[i] ,"-o",filesInEpub[i-1],NULL};
+            //execlp("pandoc", argv[i] ,"-o",filesEmEpub[i-1],NULL); // pandoc f1.txt -o f1.epub
+            //execlp("ls"," -l",NULL);
+            execvp(args[0],args);
             
         }
-
-        pideFilhos[i - 1] = pide;
-
-        char *epub= (char*)malloc(strlen(argv[i]));
-        strcpy(epub,argv[i]);
-        changeNameToEpub(epub);
-
-        filesEmEpub[i - 1] = epub;
-
-        size_for_zip += strlen(filesEmEpub[i - 1]) + 1;
-
-        printf("[pide%d] converting %s", pide, argv[i]);
         
-        free(epub);
+        pidChilds[i - 1] = pid;
     }
     
     // Checks to see if any of the attempted file conversions resulted in an error
     for(int i = 0; i < argc - 1; i++) {
         int status;
-        pid_t erro_ou_nao_erro = waitpid(pideFilhos[i],&status,WNOHANG);
+        pid_t check_error = wait(&status);
 
-        if (erro_ou_nao_erro == 1){
+        if (check_error == 1){
             printf("there was an error while converting the files to EPUB");
             return EXIT_FAILURE;
         }
     }   
 
-
+    
     // Creates the string to then use for the "zip" command
     char* str_to_zip = (char*) malloc((14 + size_for_zip) * sizeof(char));
     strcat(str_to_zip, "zip ebooks.zip");
     for (int i = 0; i < argc - 1; i++){
         strcat(str_to_zip, " ");
-        strcat(str_to_zip, filesEmEpub[i]);
+        strcat(str_to_zip, filesInEpub[i]);
+        
     }
-
-
     // Execute zip command with created string
     system(str_to_zip);
+    
+    
+    //Deletes the repetead epub files that stay outside the zip
+   for(int i = 0; i < argc - 1; i++) {
+        char *args[]={"rm",filesInEpub[i],NULL};
+        pid_t pid=fork();
+        if(pid==0)execvp(args[0],args);
+    
+   }
 
     return 0;
 }
